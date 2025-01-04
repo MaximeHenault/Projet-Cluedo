9785e2acea1f8185386ab91da7b3165868081bde
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-01-04 18:34:13 +0100

    Update Cluedo.js

--- Cluedo.js
+++ Cluedo.js
@@ -1,14 +1,14 @@
-// Sélection des boutons et des fenêtres modales
+//Recuperations des différentes valeurs des boutons et modal (Fenêtre de règle du jeu)
 const buttons = document.querySelectorAll('.clickable-box');
 const modals = document.querySelectorAll('.modal');
 const closeButtons = document.querySelectorAll('.close-btn');
 const divAccueil = document.getElementById('Accueil');
 const divPersonnage = document.getElementById('Personnage');
 
-// Ouvrir la modale associée à chaque bouton
+
+//Permet de changer la valeur du display du modal pour l'afficher
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        // Gestion des modales associées
         const modal = document.getElementById(`modal${index + 1}`);
         if (modal) {
             modal.style.display = 'flex';
@@ -16,14 +16,14 @@ buttons.forEach((button, index) => {
     });
 });
 
-// Ajouter un événement au bouton "Lancer la partie"
+//Change la valeur du diplay pour afficher la 2ème partie de la page
 const startGameButton = document.getElementById('startGameButton');
 startGameButton.addEventListener('click', () => {
     divAccueil.style.display = "none";
     divPersonnage.style.display = "block";
 });
 
-// Fermer la modale associée au bouton "Fermer"
+//Permet de fermer le modal quand le bouton fermer est pressé
 closeButtons.forEach((btn) => {
     btn.addEventListener('click', () => {
         const modalId = btn.getAttribute('data-close');
@@ -31,7 +31,7 @@ closeButtons.forEach((btn) => {
     });
 });
 
-// Fermer la modale en cliquant en dehors de son contenu
+//Permet de ferme le modal quand on clique autour
 window.addEventListener('click', (e) => {
     modals.forEach((modal) => {
         if (e.target === modal) {
